6e672a474beb36ecca42532f34dd117312e28bbc
iZeroKim <Kimgichuru6@gmail.com>
2020-11-01 13:24:26 +0300

    Scheduled the work request with WorkManager

--- app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
+++ app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
@@ -17,8 +17,14 @@
 package com.example.android.devbyteviewer
 
 import android.app.Application
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.ExistingWorkPolicy
 import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import com.example.android.devbyteviewer.work.RefreshDataWorker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import timber.log.Timber
 import java.util.concurrent.TimeUnit
 
@@ -27,6 +33,7 @@ import java.util.concurrent.TimeUnit
  */
 class DevByteApplication : Application() {
 
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
     /**
      * onCreate is called before the first screen is shown to the user.
      *
@@ -35,13 +42,28 @@ class DevByteApplication : Application() {
      */
     override fun onCreate() {
         super.onCreate()
-        Timber.plant(Timber.DebugTree())
+        delayedInit()
     }
 
     /**
      * Setup WorkManager background job to 'fetch' new network data daily.
      */
     private fun setupRecurringWork(){
-        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS)
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS).build()
+
+        Timber.d("Periodic Work request for sync is scheduled")
+
+        WorkManager.getInstance(this).enqueueUniquePeriodicWork(
+                RefreshDataWorker.WORK_NAME,
+                ExistingPeriodicWorkPolicy.KEEP,
+                repeatingRequest
+        )
+    }
+
+    private fun delayedInit(){
+        applicationScope.launch {
+            Timber.plant(Timber.DebugTree())
+            setupRecurringWork()
+        }
     }
 }
