8b81dea8fbd06ce7d055e69c3832678848c3ede3
iZeroKim <Kimgichuru6@gmail.com>
2020-11-01 15:47:37 +0300

    Updated network error message

--- app/src/main/java/com/example/android/devbyteviewer/ui/DevByteFragment.kt
+++ app/src/main/java/com/example/android/devbyteviewer/ui/DevByteFragment.kt
@@ -138,7 +138,7 @@ class DevByteFragment : Fragment() {
      */
     private fun onNetworkError() {
         if(!viewModel.isNetworkErrorShown.value!!) {
-            Toast.makeText(activity, "Network Error", Toast.LENGTH_LONG).show()
+            Toast.makeText(activity, "Not connected", Toast.LENGTH_LONG).show()
             viewModel.onNetworkErrorShown()
         }
     }
